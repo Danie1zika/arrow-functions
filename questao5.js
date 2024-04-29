fea146bd9f52643c68d3a337dffb8d245687bf52
Danie1zika <danielbrandao03420@gmail.com>
2024-04-29 11:21:32 -0400

    questao5

---
+++ questao5.js
@@ -0,0 +1,8 @@
+/**Questão 05: Escreva uma arrow function chamada maiorNumero que recebe dois 
+números como parâmetros e retorna o maior deles. */
+
+
+const maiorNumero = (a, b) => {
+    return Math.max(a, b) // Retorna a soma dos parâmetros
+}
+console.log(maiorNumero(50, 10));
