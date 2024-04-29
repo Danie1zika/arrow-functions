26166b819b3b9b61eac17f5afa5d11232d3142f9
Danie1zika <danielbrandao03420@gmail.com>
2024-04-29 11:21:12 -0400

    questao1

---
+++ questao1.js
@@ -0,0 +1,7 @@
+/**Questão 01: Escreva uma arrow function chamada dobro que recebe um número 
+como parâmetro e retorna o dobro desse número. */
+
+function soma (a, b) {
+    return a * b; 
+}
+console.log(soma(10, 2));
